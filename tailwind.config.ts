601232946891b6b67531a201e74527176a8f39dd
Natalia Mellado <nataliamelladocani@gmail.com>
2024-07-07 14:07:44 -0400

    Added styles into tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { redirect } from "next/dist/server/api-utils";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -9,7 +10,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // 
+      color: {
+        black: "#000112",
+        veryDarkGrey: "#20212C",
+        darkGrey: "#2B2C37",
+        linesDark: "#3E3F4E",
+        linesLight: "#E4EBFA",
+        mediumGrey: "#828FA3",
+        lightGrey: "#F4F7FD",
+        white: "#FFFFFF",
+        mainPurple: "#635FC7",
+        mainPurpleHover: "#A8A4FF",
+        red: "#EA5555",
+        redHover: "#FF9898",
+      },
+      fontSize: {
+        "headingXL": "24px",
+        "headingL": "18px",
+        "headingM": "15px",
+        "headingS": "12px",
+        "bodyL": "13px",
+        "bodyM": "12px",
+      },
+      letterSpacing: {
+        "headingS": "2.4px",
+      },
+      lineHeight: {
+        "bodyL": "23px",
+      },
     },
   },
   plugins: [],
